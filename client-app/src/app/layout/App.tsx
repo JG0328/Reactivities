40ec9b0c8916409e64c4e56e857c787acca2dcc5
José Gabriel González <jgg032899@gmail.com>
2020-03-11 23:12:38 -0400

    Styling the Navbar

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,29 +1,32 @@
 import React, { useState, useEffect } from "react";
-import { List } from 'semantic-ui-react';
+import { List, Container } from "semantic-ui-react";
 import axios from "axios";
 import { IActivity } from "../models/activity";
 import NavBar from "../../features/nav/NavBar";
 
 const App = () => {
-    const [activities, setActivities] = useState<IActivity[]>([]);
+  const [activities, setActivities] = useState<IActivity[]>([]);
 
-    useEffect(() => {
-        axios.get<IActivity[]>('http://localhost:5000/api/activities')
-            .then(response => {
-                setActivities(response.data);
-            });
-    }, []);
+  useEffect(() => {
+    axios
+      .get<IActivity[]>("http://localhost:5000/api/activities")
+      .then(response => {
+        setActivities(response.data);
+      });
+  }, []);
 
-    return (
-        <div>
-            <NavBar />
-            <List>
-                {activities.map((activity) => (
-                    <List.Item key={activity.id}>{activity.title}</List.Item>
-                ))}
-            </List>
-        </div>
-    );
-}
+  return (
+    <React.Fragment>
+      <NavBar />
+      <Container style={{ marginTop: "7em" }}>
+        <List>
+          {activities.map(activity => (
+            <List.Item key={activity.id}>{activity.title}</List.Item>
+          ))}
+        </List>
+      </Container>
+    </React.Fragment>
+  );
+};
 
 export default App;

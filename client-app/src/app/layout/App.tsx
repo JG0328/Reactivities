69d656b8292be436d85bd9b11dd5fd29f5f89f94
José Gabriel González <jgg032899@gmail.com>
2020-11-18 15:15:13 -0400

    Adding modals

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,36 +1,54 @@
-import React, {Fragment} from "react";
-import {Container} from "semantic-ui-react";
+import React, { Fragment, useContext, useEffect } from "react";
+import { Container } from "semantic-ui-react";
 import NavBar from "../../features/nav/NavBar";
-import {observer} from 'mobx-react-lite';
-import {Route, withRouter, RouteComponentProps, Switch} from 'react-router-dom';
+import { observer } from 'mobx-react-lite';
+import { Route, withRouter, RouteComponentProps, Switch } from 'react-router-dom';
 import HomePage from "../../features/HomePage";
 import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
 import ActivityForm from "../../features/activities/form/ActivityForm";
 import ActivityDetails from "../../features/activities/details/ActivityDetails";
 import NotFound from "./NotFound";
-import {ToastContainer} from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
 import LoginForm from "../../features/user/LoginForm";
+import { RootStoreContext } from "../stores/rootStore";
+import LoadingComponent from "./LoadingComponent";
+import ModalContainer from "../common/modals/ModalContainer";
 
-const App: React.FC<RouteComponentProps> = ({location}) => {
+const App: React.FC<RouteComponentProps> = ({ location }) => {
+    const rootStore = useContext(RootStoreContext);
+    const { setAppLoaded, token, appLoaded } = rootStore.commonStore;
+    const { getUser } = rootStore.userStore;
+
+    useEffect(() => {
+        if (token) {
+            getUser().finally(() => setAppLoaded());
+        } else {
+            setAppLoaded();
+        }
+    }, [getUser, setAppLoaded, token]);
+
+    if (!appLoaded) return <LoadingComponent content={"Loading app..."} />
+
     return (
         <React.Fragment>
-            <ToastContainer position="bottom-right"/>
-            <Route exact path={'/'} component={HomePage}/>
+            <ModalContainer />
+            <ToastContainer position="bottom-right" />
+            <Route exact path={'/'} component={HomePage} />
             <Route path={'/(.+)'} render={() => (
                 <Fragment>
-                    <NavBar/>
-                    <Container style={{marginTop: "7em"}}>
+                    <NavBar />
+                    <Container style={{ marginTop: "7em" }}>
                         <Switch>
-                            <Route exact path={'/activities'} component={ActivityDashboard}/>
-                            <Route path={'/activities/:id'} component={ActivityDetails}/>
+                            <Route exact path={'/activities'} component={ActivityDashboard} />
+                            <Route path={'/activities/:id'} component={ActivityDetails} />
                             <Route key={location.key} path={['/createActivity', '/manage/:id']}
-                                   component={ActivityForm}/>
-                            <Route path={'/login'} component={LoginForm}/>
-                            <Route component={NotFound}/>
+                                component={ActivityForm} />
+                            <Route path={'/login'} component={LoginForm} />
+                            <Route component={NotFound} />
                         </Switch>
                     </Container>
                 </Fragment>
-            )}/>
+            )} />
         </React.Fragment>
     );
 };
--- client-app/src/features/user/LoginForm.tsx
+++ client-app/src/features/user/LoginForm.tsx
@@ -1,6 +1,6 @@
 ﻿import React, { useContext } from "react";
 import { Form as FinalForm, Field } from 'react-final-form';
-import { Button, Form, Label } from "semantic-ui-react";
+import { Button, Form, Header, Label } from "semantic-ui-react";
 import TextInput from "../../app/common/form/TextInput";
 import { RootStoreContext } from "../../app/stores/rootStore";
 import { IUserFormValues } from "../../app/models/user";
@@ -22,8 +22,9 @@ const LoginForm = () => {
                 [FORM_ERROR]: error
             }))}
             validate={validate}
-            render={({ handleSubmit, submitting, form, submitError, invalid, pristine, dirtySinceLastSubmit }) => (
+            render={({ handleSubmit, submitting, submitError, invalid, pristine, dirtySinceLastSubmit }) => (
                 <Form onSubmit={handleSubmit}>
+                    <Header as={"h2"} content={"Login to Reactivities"} color={"teal"} textAlign={"center"} />
                     <Field
                         name={'email'}
                         component={TextInput}
@@ -36,9 +37,7 @@ const LoginForm = () => {
                         type={"password"}
                     />
                     {submitError && !dirtySinceLastSubmit && (<Label color={"red"} basic content={submitError.statusText} />)}
-                    <br />
-                    <Button disabled={invalid && !dirtySinceLastSubmit || pristine} loading={submitting} positive content={"Login"} />
-                    <pre>{JSON.stringify(form.getState(), null, 2)}</pre>
+                    <Button color={"teal"} disabled={invalid && !dirtySinceLastSubmit || pristine} loading={submitting} positive content={"Login"} fluid />
                 </Form>
             )}
         />
